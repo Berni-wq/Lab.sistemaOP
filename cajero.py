894e4a9b0e1a1d5279838d6322a8bf43a6491c49
esteban cortés <166844979+estebanuls@users.noreply.github.com>
2026-05-04 16:41:16 -0400

    se arregla error de comentado en semaforo

--- cajero.py
+++ cajero.py
@@ -9,7 +9,7 @@ import time
 #SEMÁFORO: controla el acceso al saldo compartido.
 #Valor inicial 1 = solo UN hilo puede operar a la vez.
 #Si fuera 2, dos hilos podrían modificar el saldo al mismo tiempo o sea error.
-#semaforo = threading.Semaphore(1)
+semaforo = threading.Semaphore(1)
 
 saldo = 1000.0        # saldo inicial compartido entre ambos hilos
 operaciones = []      # lista compartida: el productor escribe, el consumidor lee
