1755c260d1d54af14352c0e087eeff9b1e7e2436
Berni-wq <bcasangagallardo@gmail.com>
2026-05-03 22:14:11 -0400

    inicializacion
    de variables y las librerias a utilizar

--- cajero.py
+++ cajero.py
@@ -1,1 +1,17 @@
+#Grupo: 04 | Técnica: Semáforos (threading.Semaphore)
+#Integrantes: Bernardo Casanga, Maximiliano Uribe,
+#Esteban Cortés, Felipe Segura
+#Asignatura: Sistemas Operativos | Universidad La Serena
+import threading
+import random
+import time
 
+#SEMÁFORO: controla el acceso al saldo compartido.
+#Valor inicial 1 = solo UN hilo puede operar a la vez.
+#Si fuera 2, dos hilos podrían modificar el saldo al mismo tiempo o sea error.
+#semaforo = threading.Semaphore(1)
+
+saldo = 1000.0        # saldo inicial compartido entre ambos hilos
+operaciones = []      # lista compartida: el productor escribe, el consumidor lee
+NUM_OPERACIONES = 8
+archivo_salida = "resultados.txt"
